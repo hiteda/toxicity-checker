aa96b7ad8086171cbb96e4a96fe87a587a62aabc
David K Hite <david.k.hite@gmail.com>
2019-09-18 14:16:33 -0700

    Added algorithmic functionality, added some stuff to the About page

--- src/about.js
+++ src/about.js
@@ -4,10 +4,17 @@ class About extends React.Component {
     render() {
         console.log(this.props)
         return (
-            <div className="App">
-              <header className="App-header">
-                <h1>About Page</h1>
-              </header>
+            <div className="container">
+              <div className="center jumbotron">
+                <h1>How Does it Work?</h1>
+                <p>Using science and cutting-edge algorithmic technology, the Toxicity Checker creates a numeric hash of the input and uses that hash to assign a genuine toxicity rating.</p>
+                <h2>"It's Mathemagical!" -Dr. Tae Kashawa</h2>
+              </div>
+              <div className="center jumbotron">
+                  <h1>Is it Accurate?</h1>
+                  <p>"Sure!"</p>
+                  <h2>"I'd trust it as far as I can throw it." -Dr. Ima Real</h2>
+              </div>
             </div>
           );
     }
