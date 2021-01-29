5ea30fcd2d61841487888df1222e0b1bf77644b7
David Hite <hiteda@users.noreply.github.com>
2021-01-28 21:31:28 -0800

    Feature/testing (#1)
    * Added integration tests
    
    * Add github CI action
    
    * Fix test run description

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,39 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import { render, screen, fireEvent } from '@testing-library/react'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('App', () => {
+  test('renders without crashing', () => {
+    render(<App/>);
+  });
+
+  test('shows results', () => {
+    render(<App/>);
+    fireEvent.change(screen.getByRole('textbox'), {
+      target: {value: 'Foo'}
+    });
+    fireEvent.click(screen.getByRole('button'));
+    screen.getByText(/Toxicity Rating:/);
+  });
+
+  test('shows no results when input is empty', () => {
+    render(<App/>);
+    // Check clicking submit with no input
+    fireEvent.click(screen.getByRole('button'));
+    expect(screen.queryByText(/Toxicity Rating:/)).toBeNull();
+
+    // Should show result when input is not empty
+    fireEvent.change(screen.getByRole('textbox'), {
+      target: {value: 'Foo'}
+    });
+    fireEvent.click(screen.getByRole('button'));
+    screen.getByText(/Toxicity Rating:/);
+    
+    // Should show no results element when input is empty again
+    fireEvent.change(screen.getByRole('textbox'), {
+      target: {value: ''}
+    });
+    fireEvent.click(screen.getByRole('button'));
+    expect(screen.queryByText(/Toxicity Rating:/)).toBeNull();
+  });
 });
